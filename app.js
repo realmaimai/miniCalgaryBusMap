9742babc528bb09cfac68fb6b8eb84ecc46e4b1a
yulez <yule919@gmail.com>
2024-01-29 16:33:02 -0700

    add cool feature

--- app.js
+++ app.js
@@ -1,6 +1,97 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoieXVsZXpoIiwiYSI6ImNscmtkMmR0YjBkY2gya28yM3ZobXp2eTQifQ.7zqzJNm7ZrAJdzJNDpEt5w';
 
-const theme = determineTheme();
+let theme = determineTheme();
+
+let map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/yulezh/clrkd76ck001301pqhgku01rq/draft', // style URL
+        center: [-114.062925,51.044312], // starting position [lng, lat]
+        zoom: 15.5, // starting zoom
+        pitch: 45,
+        maxZoom: 18,
+        minZoom:11,
+        maxBounds: [
+            [-114.238754,50.864202], // Southwest coordinates
+            [-113.796794,51.208483] // Northeast coordinates
+          ]
+    });
+
+updateBus(map);
+map.addControl(new mapboxgl.NavigationControl());
+
+// create threebox instance
+const tb = (window.tb = new Threebox(
+    map,
+    map.getCanvas().getContext('webgl'),
+    {
+    defaultLights: true
+    }
+    ));
+
+// load map
+map.on('load', () => {
+
+    map.setConfigProperty('basemap', 'showTransitLabel', false)
+    determineLightPreset(theme.preset);
+    
+    // add 3d bus model
+    map.addLayer({
+        id: 'bus-3d-model',
+        type: 'custom',
+        renderingMode: '3d',
+        onAdd: function () {
+            const scale = 2000;
+            const options = {
+            obj: 'static/model/red-bus.glb',
+            type: 'glb',
+            scale: { x: scale, y: scale, z: scale },
+            units: 'meters',
+            rotation: { x: 90, y: -90, z: 0 },
+            anchor: 'center'
+        };
+            
+        const transitData= map.getSource('calgary-transit-position');
+        const features = transitData._data.features;
+        features.forEach((feature)=> {
+            tb.loadObj(options, (model) => {
+                const position = feature.geometry.coordinates;
+                position.push(1);
+                model.setCoords(position);
+                tb.add(model);
+                });
+            });
+        },
+            
+        render: function () {
+            tb.update();
+        }
+    })
+    map.setLayerZoomRange('bus-3d-model', 14,30);
+
+    const layers = map.getStyle().layers
+    const layerList = [];
+    layers.forEach(element => {
+        layerList.push(element.id)
+    })
+    
+    layerList.forEach(layerName => {
+        map.on('click', layerName, function (e) {
+            // Retrieve the features under the clicked location
+            const features = map.queryRenderedFeatures(e.point);
+            
+            // If there are any features, create a popup and display the information
+            if (features.length > 0) {
+                const feature = features[0]; // Get the first feature
+                // Create a popup instance
+                const popup = new mapboxgl.Popup()
+                .setLngLat(feature.geometry.coordinates)
+                .setHTML('<h3 class="popup">' + feature.properties.title + '</h3><p class="popup">' + 'Update time: ' + feature.properties.update_time + '</p>')
+                .addTo(map);
+            }
+            });
+    })
+})
+
 
 async function updateBus(map) {
     try {
@@ -32,69 +123,6 @@ async function updateBus(map) {
         console.log("update bus error:", error);
     }
 }
-
-const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/yulezh/clrkd76ck001301pqhgku01rq/draft', // style URL
-        center: [-114.062925,51.044312], // starting position [lng, lat]
-        zoom: 15.5, // starting zoom
-        pitch: 45,
-    });
-
-
-    updateBus(map);
-    
-    // create threebox instance
-    const tb = (window.tb = new Threebox(
-        map,
-        map.getCanvas().getContext('webgl'),
-        {
-        defaultLights: true
-        }
-        ));
-
-    // load map
-    map.on('load', () => {
-
-
-        determineLightPreset(theme.preset);
-
-        // add 3d bus model
-        map.addLayer({
-            id: 'custom-threebox-model',
-            type: 'custom',
-            renderingMode: '3d',
-            onAdd: function () {
-                const scale = 0.2;
-                const options = {
-                obj: 'static/model/Bus.glb',
-                type: 'glb',
-                scale: { x: scale, y: scale, z: scale },
-                units: 'meters',
-                rotation: { x: 90, y: -90, z: 0 },
-                anchor: 'center'
-            };
-             
-            const transitData= map.getSource('calgary-transit-position');
-            const features = transitData._data.features;
-            features.forEach((feature)=> {
-                tb.loadObj(options, (model) => {
-                    const position = feature.geometry.coordinates;
-                    position.push(-1)
-                    model.setCoords(position);
-                    tb.add(model);
-                    });
-                });
-            },
-             
-            render: function () {
-                tb.update();
-            }
-        })
-
-    })
-
-
 
 async function logBuses() {
     const response = await fetch('http://localhost:8080', {
@@ -141,7 +169,7 @@ function determineTheme() {
                 'theme': 'light',
                 'preset': 'day'
             }
-            return theme;
+        return theme;
     }
 }
 
@@ -170,21 +198,54 @@ function mapAddLayer(map, theme) {
                     'source': 'calgary-transit-position', // reference the data source
                     'layout': {
                         'icon-image': 'red-bus', // reference the image
-                        'icon-size': 0.5
+                        'icon-size': 0.25
                     }
                 })
-            } else {
                 map.addLayer({
-                    'id': 'calgary-transit-position-point',
+                    'id': 'calgary-transit-position-under',
+                    'type': 'circle',
+                    'source': 'calgary-transit-position', // reference the data source
+                    'paint': {
+                        'circle-radius': 6,
+                        'circle-color': '#B32233',
+                        'circle-stroke-width': 2,
+                        'circle-stroke-color': '#fff',
+                        'circle-blur': 0.2,
+                    }
+                })
+                map.setLayerZoomRange('calgary-transit-position-point', 0,14);
+                map.setLayerZoomRange('calgary-transit-position-under', 14,22);
+            } else if (theme.theme == 'dark') {
+                map.addLayer({
+                    'id': 'calgary-transit-position-point-dark',
                     'type': 'symbol',
                     'source': 'calgary-transit-position', // reference the data source
                     'layout': {
                         'icon-image': 'gray-bus', // reference the image
                         'icon-size': 0.25
                     }
-            })
+                })
+                map.addLayer({
+                    'id': 'calgary-transit-position-under-dark',
+                    'type': 'circle',
+                    'source': 'calgary-transit-position', // reference the data source
+                    'paint': {
+                        'circle-radius': 6,
+                        'circle-color': '#E76372',
+                        'circle-stroke-width': 2,
+                        'circle-stroke-color': '#ffffff',
+                        'circle-blur': 0.2,
+                        'circle-emissive-strength': 0.8
+                    }
+                })
+                map.setLayerZoomRange('calgary-transit-position-point-dark', 0,14);
+                map.setLayerZoomRange('calgary-transit-position-under-dark', 14,22);
         }
     }
-function determineWeather(){
-    return
-}
+
+document.querySelectorAll('.map-overlay-inner input[type="checkbox"]')
+        .forEach((checkbox) => {
+            checkbox.addEventListener('change', function () {
+            map.setConfigProperty('basemap', this.id, checked);
+            });
+        });
