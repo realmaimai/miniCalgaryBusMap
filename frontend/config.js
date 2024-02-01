f77bec1f8e0180aeaaf21f4cd8add056b5aa2c77
yulez <yule919@gmail.com>
2024-02-01 03:23:28 -0700

    update README

--- frontend/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    mapboxAccessToken: 'pk.eyJ1IjoieXVsZXpoIiwiYSI6ImNscmtkMmR0YjBkY2gya28yM3ZobXp2eTQifQ.7zqzJNm7ZrAJdzJNDpEt5w',
-    openWeatherToken: 'e785eb712b60c70bdb3f905c6e3cb2de',
-    mapboxStyle: 'mapbox://styles/yulezh/clrkd76ck001301pqhgku01rq/draft',
-}
-
-export default config;
