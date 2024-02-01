46da4b002054c6fa3af22be88c5010a3392c1991
yulez <yule919@gmail.com>
2024-02-01 02:05:47 -0700

    change structure

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+const config = {
+    mapboxAccessToken: 'pk.eyJ1IjoieXVsZXpoIiwiYSI6ImNscmtkMmR0YjBkY2gya28yM3ZobXp2eTQifQ.7zqzJNm7ZrAJdzJNDpEt5w',
+    openWeatherToken: 'db804e4c5e9a18e8fc19bf06c3680a05',
+    mapboxStyle: 'mapbox://styles/yulezh/clrkd76ck001301pqhgku01rq/draft',
+}
+
+export default config;
