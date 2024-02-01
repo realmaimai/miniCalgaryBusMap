a6f60b717569dea115ea6f9b700437c3de59e8ec
yulezh <yule919@gmail.com>
2024-02-01 02:17:54 -0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 const config = {
-    mapboxAccessToken: 'pk.eyJ1IjoieXVsZXpoIiwiYSI6ImNscmtkMmR0YjBkY2gya28yM3ZobXp2eTQifQ.7zqzJNm7ZrAJdzJNDpEt5w',
-    openWeatherToken: 'db804e4c5e9a18e8fc19bf06c3680a05',
-    mapboxStyle: 'mapbox://styles/yulezh/clrkd76ck001301pqhgku01rq/draft',
+    mapboxAccessToken: your Mapbox token,
+    openWeatherToken: your openWeather token,
+    mapboxStyle: your Mapbox style link,
 }
 
 export default config;
